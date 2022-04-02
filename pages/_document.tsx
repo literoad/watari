a2752218f24d12d39098a83c0405b3c2811274b0
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2022-04-02 20:29:51 +0300

    update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,25 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Bitter:wght@400;700&family=Righteous&display=swap"
           rel="stylesheet"
         />
-        <link rel="icon" sizes="32x32" href="/images/favicon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#63d8c4" />
       </Head>
       <body>
         <Main />
