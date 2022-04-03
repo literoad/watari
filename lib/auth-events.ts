9429a8397b8471caee1745470ee3e09cc39c2620
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2022-04-03 08:12:14 +0300

    report user creation to shingo

--- lib/auth-events.ts
+++ lib/auth-events.ts
@@ -1,3 +1,12 @@
 import { User } from "next-auth";
+import fetch from "node-fetch";
 
-export async function onCreateUser({ user }: { user: User }) {}
+export async function onCreateUser({ user }: { user: User }) {
+  await fetch(`${process.env.SHINGO_URL}/users`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ user }),
+  });
+}
