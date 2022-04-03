95b2e80320f2ed24a0c419c7835f734e961063be
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2022-04-03 17:38:11 +0300

    use full-width graph

--- pages/monitors/[id].tsx
+++ pages/monitors/[id].tsx
@@ -5,6 +5,7 @@ import {
   Legend,
   Line,
   LineChart,
+  ResponsiveContainer,
   Tooltip,
   XAxis,
   YAxis,
@@ -36,31 +37,31 @@ const MonitorPage: NextPage<Props> = ({ monitor, measurements }) => {
       <section className="lr-container text-center">
         <h2>Монитор {monitor.url}</h2>
         <div className={s.chart}>
-          <LineChart
-            width={800}
-            height={600}
-            data={measurements}
-            margin={{ left: 30, right: 40, top: 30, bottom: 30 }}
-          >
-            <CartesianGrid stroke="#ccc" />
-            <XAxis dataKey="_id" />
-            <YAxis domain={[0, 100]} />
-            <Line
-              name="Performance"
-              dataKey="performance"
-              stroke="#eb512b"
-              strokeWidth={2}
-            />
-            <Line
-              name="Best Practices"
-              dataKey="bestPractices"
-              stroke="#bdc921"
-              strokeWidth={2}
-            />
-            <Line name="SEO" dataKey="seo" stroke="#91c4e3" strokeWidth={2} />
-            <Tooltip contentStyle={{ backgroundColor: "#444" }} />
-            <Legend />
-          </LineChart>
+          <ResponsiveContainer height={600} width="100%">
+            <LineChart
+              data={measurements}
+              margin={{ left: 50, right: 50, top: 50, bottom: 50 }}
+            >
+              <CartesianGrid stroke="#ccc" />
+              <XAxis dataKey="_id" />
+              <YAxis domain={[0, 100]} />
+              <Line
+                name="Performance"
+                dataKey="performance"
+                stroke="#eb512b"
+                strokeWidth={2}
+              />
+              <Line
+                name="Best Practices"
+                dataKey="bestPractices"
+                stroke="#bdc921"
+                strokeWidth={2}
+              />
+              <Line name="SEO" dataKey="seo" stroke="#91c4e3" strokeWidth={2} />
+              <Tooltip contentStyle={{ backgroundColor: "#333" }} />
+              <Legend />
+            </LineChart>
+          </ResponsiveContainer>
         </div>
       </section>
     </div>
