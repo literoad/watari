23b859e1b12c5cfb076b756c52a879acb1f36f72
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2022-04-08 20:50:14 +0300

    add bitrix24 support chat form

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -51,6 +51,18 @@ export default function Document() {
             />
           </div>
         </noscript>
+        {/* Bitrix chat form */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function(w,d,u){
+                    var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+                    var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+            })(window,document,'https://cdn-ru.bitrix24.ru/b21045948/crm/site_button/loader_3_67gb2m.js');
+        `,
+          }}
+        />
       </Head>
       <body>
         <Main />
